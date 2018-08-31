b22418e91b34ee7e4305efadfe9429dae690e1f2
Daniel Bright <danielcbright@gmail.com>
2018-08-31 11:57:13 -0400

    fixed ssl cert stuff

--- vars/runChefEnvJob.groovy
+++ vars/runChefEnvJob.groovy
@@ -5,6 +5,8 @@ def call() {
         sh('chmod +x envFunctions.rb')
         sh('ls -alt')
         sh 'knife ssl fetch'
+        env.SSL_CERT_DIR = "${workspace}/.chef/trusted_certs/"
+        sh 'env'
         sh 'chef exec ruby envFunctions.rb -k .chef/knife.rb'
     }
 }
