a99296c37141cb62ab9b538a10e88c7bf5d86067
Daniel Bright <danielcbright@gmail.com>
2018-08-31 09:45:33 -0400

    almost there

--- vars/knifeNodeList.groovy
+++ vars/knifeNodeList.groovy
@@ -1,6 +1,7 @@
 def call() {
     node {
         wrap([$class: 'ChefIdentityBuildWrapper', jobIdentity: 'Jenkins']) {
+        sh 'knife ssl fetch'
         sh 'knife node list -c .chef/knife.rb'
         }
     }
