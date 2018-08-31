65d7b41ab5344976a52af9a8bcb06a8c617f728e
Daniel Bright <danielcbright@gmail.com>
2018-08-31 11:37:34 -0400

    removed node wrapp

--- vars/runChefEnvJob.groovy
+++ vars/runChefEnvJob.groovy
@@ -1,12 +1,10 @@
 def call() {
-    node {
-        wrap([$class: 'ChefIdentityBuildWrapper', jobIdentity: 'Jenkins']) {
-            def rubyContent = libraryResource('envFunctions.rb')
-            writeFile(file: 'envFunctions.rb', text: rubyContent)
-            sh('chmod +x envFunctions.rb')
-            sh('ls -alt')
-            sh 'knife ssl fetch'
-            sh 'chef exec ruby envFunctions.rb -k .chef/knife.rb'
-        }
+    wrap([$class: 'ChefIdentityBuildWrapper', jobIdentity: 'Jenkins']) {
+        def rubyContent = libraryResource('envFunctions.rb')
+        writeFile(file: 'envFunctions.rb', text: rubyContent)
+        sh('chmod +x envFunctions.rb')
+        sh('ls -alt')
+        sh 'knife ssl fetch'
+        sh 'chef exec ruby envFunctions.rb -k .chef/knife.rb'
     }
 }
