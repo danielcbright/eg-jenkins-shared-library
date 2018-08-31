6b5f9c9922e7ac40e8f6072a7aa287e0328ff15d
Daniel Bright <danielcbright@gmail.com>
2018-08-31 11:34:31 -0400

    fixed load order of sh

--- vars/runChefEnvJob.groovy
+++ vars/runChefEnvJob.groovy
@@ -1,12 +1,12 @@
 def call() {
     node {
         wrap([$class: 'ChefIdentityBuildWrapper', jobIdentity: 'Jenkins']) {
-            sh 'knife ssl fetch'
-            sh 'chef exec ruby envFunctions.rb -k .chef/knife.rb'
             def rubyContent = libraryResource('envFunctions.rb')
             writeFile(file: 'envFunctions.rb', text: rubyContent)
             sh('chmod +x envFunctions.rb')
             sh('ls -alt')
+            sh 'knife ssl fetch'
+            sh 'chef exec ruby envFunctions.rb -k .chef/knife.rb'
         }
     }
 }
