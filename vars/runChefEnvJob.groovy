825c79865775b110375fc8ae63f7238cb52f4760
Daniel Bright <danielcbright@gmail.com>
2018-08-31 11:31:03 -0400

    test

--- vars/runChefEnvJob.groovy
+++ vars/runChefEnvJob.groovy
@@ -1,8 +1,9 @@
 def call() {
-    def rubyContent = libraryResource('envFunctions.rb')
-    writeFile(file: 'envFunctions.rb', text: rubyContent)
-    sh('chmod +x envFunctions.rb')
     node {
+        def rubyContent = libraryResource('envFunctions.rb')
+        writeFile(file: 'envFunctions.rb', text: rubyContent)
+        sh('chmod +x envFunctions.rb')
+        sh('ls -alt')
         wrap([$class: 'ChefIdentityBuildWrapper', jobIdentity: 'Jenkins']) {
         sh 'knife ssl fetch'
         sh 'chef exec ruby envFunctions.rb -k .chef/knife.rb'
