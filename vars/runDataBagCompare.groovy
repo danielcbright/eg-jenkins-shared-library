a74c3f383490c7a8c075e41e11fa222734689b37
Daniel Bright <danielcbright@gmail.com>
2018-09-04 17:51:29 -0400

    added stash stuff

--- vars/runDataBagCompare.groovy
+++ vars/runDataBagCompare.groovy
@@ -1,5 +1,6 @@
 def call() {
     wrap([$class: 'ChefIdentityBuildWrapper', jobIdentity: 'Jenkins']) {
+        unstash 'sslCert'
         env.SSL_CERT_FILE= "${workspace}/.chef/trusted_certs/chef-server_dbright_io.crt"
         sh "ls -alt ${workspace}/.chef/trusted_certs/ && ls -alt && env && chef exec ruby envFunctions.rb -k .chef/knife.rb -c"
     }
