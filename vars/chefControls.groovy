645f81a4db3f2406c0cfa7d810b0b0eea87cdadd
Daniel Bright <danielcbright@gmail.com>
2018-09-04 14:05:26 -0400

    fixed brackets

--- vars/chefControls.groovy
+++ vars/chefControls.groovy
@@ -2,29 +2,31 @@
 
 def call(){
   pipeline {
-    agent any
-    stages {
-      parallel {
-        stage('Stage Environments') {
-          steps {
-            runChefEnvJobCompare()
+      agent any
+        stages {
+          parallel {
+            stage('Stage Environments') {
+              steps {
+                runChefEnvJobCompare()
+              }
+            }
+            state('Stage Data Bags') {
+              steps (
+                runDataBagCompare()
+              )
+            }
+        stage('Publish Environments to Production') {
+          parallel {
+            steps {
+              input 'Publish Environments to Production Chef Server?'
+              runChefEnvJobProcess()
+            }
+            steps {
+              input 'Publish Data Bags to Production Chef Server?'
+              runDataBagProcess()
+            }
           }
         }
-        state('Stage Data Bags') {
-          steps (
-            runDataBagCompare()
-          )
-        }
-      stage('Publish Environments to Production') {
-        parallel {
-          steps {
-            input 'Publish Environments to Production Chef Server?'
-            runChefEnvJobProcess()
-          }
-          steps {
-            input 'Publish Data Bags to Production Chef Server?'
-            runDataBagProcess()
-          }
       }
     }
   }
