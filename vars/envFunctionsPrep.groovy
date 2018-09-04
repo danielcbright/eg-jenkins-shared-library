2d6dad968813993b4761f0db1ad00858039e23c6
Daniel Bright <danielcbright@gmail.com>
2018-09-04 17:22:09 -0400

    troubleshooting

--- vars/envFunctionsPrep.groovy
+++ vars/envFunctionsPrep.groovy
@@ -2,7 +2,7 @@ def call() {
     wrap([$class: 'ChefIdentityBuildWrapper', jobIdentity: 'Jenkins']) {
         def rubyContent = libraryResource('envFunctions.rb')
         writeFile(file: 'envFunctions.rb', text: rubyContent)
-        sh 'knife ssl fetch'
+        sh 'ls -alt && env && knife ssl fetch'
         env.SSL_CERT_FILE= "${workspace}/.chef/trusted_certs/chef-server_dbright_io.crt"
     }
 }
