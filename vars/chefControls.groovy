b5f8ecbcd382d30dd32c7d14f52c1a75c983aece
Daniel Bright <danielcbright@gmail.com>
2018-09-04 14:49:04 -0400

    fixed nesting

--- vars/chefControls.groovy
+++ vars/chefControls.groovy
@@ -4,29 +4,33 @@ def call() {
   pipeline {
     agent any
     stages {
-      parallel {
-        stage('Stage Environments') {
-          steps {
-            runChefEnvJobCompare()
+      stage('Prepare Chef Objects') {
+        parallel {
+          stage('Stage Environments') {
+            steps {
+              runChefEnvJobCompare()
+            }
           }
-        }
-        stage('Stage Data Bags') {
-          steps (
-            runDataBagCompare()
-          )
+          stage('Stage Data Bags') {
+            steps (
+              runDataBagCompare()
+            )
+          }
         }
       }
-      parallel {
-        stage('Publish Environments to Production') {
-          steps {
-            input 'Publish Environments to Production Chef Server?'
-            runChefEnvJobProcess()
+      stage('Approve & Publish Changes') {
+        parallel {
+          stage('Publish Environments to Production') {
+            steps {
+              input 'Publish Environments to Production Chef Server?'
+              runChefEnvJobProcess()
+            }
           }
-        }
-        stage('Publish Data Bags to Production') {
-          steps {
-            input 'Publish Data Bags to Production Chef Server?'
-            runDataBagProcess()
+          stage('Publish Data Bags to Production') {
+            steps {
+              input 'Publish Data Bags to Production Chef Server?'
+              runDataBagProcess()
+            }
           }
         }
       }
