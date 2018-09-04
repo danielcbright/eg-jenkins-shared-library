8c6c48b99dcbcfec5176c0a800c4e3d435c12e5d
Daniel Bright <danielcbright@gmail.com>
2018-09-04 17:52:45 -0400

    fixed path

--- vars/envFunctionsPrep.groovy
+++ vars/envFunctionsPrep.groovy
@@ -3,6 +3,6 @@ def call() {
         def rubyContent = libraryResource('envFunctions.rb')
         writeFile(file: 'envFunctions.rb', text: rubyContent)
         sh "knife ssl fetch"
-        stash includes: "${workspace}/.chef/trusted_certs/**", name: 'sslCert'
+        stash includes: ".chef/trusted_certs/**", name: 'sslCert'
     }
 }
