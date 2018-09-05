9dd68b8435456a940eac5f6800ccf057c6983133
Daniel Bright <danielcbright@gmail.com>
2018-09-05 09:34:25 -0400

    making envOut global

--- vars/runChefEnvJobCompare.groovy
+++ vars/runChefEnvJobCompare.groovy
@@ -2,6 +2,6 @@ def call() {
     wrap([$class: 'ChefIdentityBuildWrapper', jobIdentity: 'Jenkins']) {
         unstash 'sslCert'
         env.SSL_CERT_FILE= "${workspace}/.chef/trusted_certs/chef-server_dbright_io.crt"
-        def envOut = sh script: 'chef exec ruby envFunctions.rb -k .chef/knife.rb -c', returnStdout: true
+        envOut = sh script: 'chef exec ruby envFunctions.rb -k .chef/knife.rb -c', returnStdout: true
     }
 }
