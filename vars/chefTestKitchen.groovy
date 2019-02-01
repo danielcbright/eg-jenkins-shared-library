bea0d15e0dfc906fdf2fc0ae3d0e153a64fd6c1f
Daniel Bright <danielcbright@gmail.com>
2019-02-01 13:55:53 -0500

    added workspace

--- vars/chefTestKitchen.groovy
+++ vars/chefTestKitchen.groovy
@@ -3,6 +3,7 @@ def call() {
     node {
         wrap([$class: 'ChefIdentityBuildWrapper', jobIdentity: 'Jenkins']) {
         sh 'ls -alt'
+        sh 'pwd'
         sh 'kitchen test ${workspace}/'
         }
     }
