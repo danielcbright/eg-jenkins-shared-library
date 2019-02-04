85dffa2fdf6d23103db0c320444e82b62ca4d5c4
Daniel Bright <danielcbright@gmail.com>
2019-02-04 09:35:17 -0500

    removed workspace attr

--- vars/chefTestKitchen.groovy
+++ vars/chefTestKitchen.groovy
@@ -1,8 +1,10 @@
 // run Test Kitchen
 def call() {
-        wrap([$class: 'ChefIdentityBuildWrapper', jobIdentity: 'Jenkins']) {
+    wrap([$class: 'ChefIdentityBuildWrapper', jobIdentity: 'Jenkins']) {
+        unstash 'sslCert'
+        env.SSL_CERT_FILE= "${workspace}/.chef/trusted_certs/chef-server_dbright_io.crt"
         sh 'ls -alt'
         sh 'pwd'
         sh 'kitchen test -d always'
-        }
+    }
 }
