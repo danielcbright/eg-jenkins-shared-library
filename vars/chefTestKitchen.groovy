aca92eef0e93bcb28974281a4b3296326d4dad2d
Daniel Bright <danielcbright@gmail.com>
2019-02-04 09:22:20 -0500

    removed workspace attr

--- vars/chefTestKitchen.groovy
+++ vars/chefTestKitchen.groovy
@@ -4,7 +4,7 @@ def call() {
         wrap([$class: 'ChefIdentityBuildWrapper', jobIdentity: 'Jenkins']) {
         sh 'ls -alt'
         sh 'pwd'
-        sh 'kitchen test ${workspace}/'
+        sh 'kitchen test'
         }
     }
 }
