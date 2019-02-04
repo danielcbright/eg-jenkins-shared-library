7598398edfe72926ed2156e03456085eca977f9b
Daniel Bright <danielcbright@gmail.com>
2019-02-04 14:23:15 -0500

    fixed

--- vars/getFileChanges.groovy
+++ vars/getFileChanges.groovy
@@ -1,7 +1,6 @@
 def call() {
     echo "Gathering SCM changes"
     def changeLogSets = currentBuild.changeSets
-    changeString = ''
     for (int i = 0; i < changeLogSets.size(); i++) {
         def entries = changeLogSets[i].items
         for (int j = 0; j < entries.length; j++) {
@@ -17,5 +16,6 @@ def call() {
     if (!changeString) {
         changeString = " - No new changes"
     }
+    echo changeString
     return changeString
 }
