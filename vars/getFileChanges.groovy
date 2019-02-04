296e4bf61b4a32d0ec940a96800e65b16fdc994d
Daniel Bright <danielcbright@gmail.com>
2019-02-04 14:14:08 -0500

    fixed file

--- vars/getFileChanges.groovy
+++ vars/getFileChanges.groovy
@@ -1,13 +1,11 @@
 def call() {
-    MAX_MSG_LEN = 100
-
     echo "Gathering SCM changes"
+    string(name: 'changeString', defaultValue: '')
     def changeLogSets = currentBuild.changeSets
     for (int i = 0; i < changeLogSets.size(); i++) {
         def entries = changeLogSets[i].items
         for (int j = 0; j < entries.length; j++) {
             def entry = entries[j]
-            def changeString = ""
             def files = new ArrayList(entry.affectedFiles)
             for (int k = 0; k < files.size(); k++) {
                 def file = files[k]
