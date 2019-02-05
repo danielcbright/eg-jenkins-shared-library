6bdb8bbdb918a77332aab3674e4a6e068a86fda0
Daniel Bright <danielcbright@gmail.com>
2019-02-05 12:39:53 -0500

    fixed

--- vars/getFileChanges.groovy
+++ vars/getFileChanges.groovy
@@ -1,12 +1,12 @@
 def call() {
     echo "Gathering SCM changes"
-    def changeString = "# This file contains a list of files changed since the last commit\n"
     def changeLogSets = currentBuild.changeSets
     for (int i = 0; i < changeLogSets.size(); i++) {
         def entries = changeLogSets[i].items
         for (int j = 0; j < entries.length; j++) {
             def entry = entries[j]
             def files = new ArrayList(entry.affectedFiles)
+            def changeString = "# This file contains a list of files changed since the last commit\n"
             for (int k = 0; k < files.size(); k++) {
                 def file = files[k]
                 changeString += " ${file.path}"
