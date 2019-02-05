6e082e9c6ce50c73ac1ffd46f1fa6fed47f9f7e0
Daniel Bright <danielcbright@gmail.com>
2019-02-05 13:48:47 -0500

    fixed

--- vars/getFileChanges.groovy
+++ vars/getFileChanges.groovy
@@ -1,7 +1,7 @@
 import groovy.transform.Field
 
 def changes() {
-    @Field String changeString = ""
+    changeString = ""
 
     echo "Gathering SCM changes"
     def changeLogSets = currentBuild.changeSets
