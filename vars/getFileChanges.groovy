21bfe54de594ea64e0534020bab942bfd75132af
Daniel Bright <danielcbright@gmail.com>
2019-02-05 13:35:55 -0500

    fixed

--- vars/getFileChanges.groovy
+++ vars/getFileChanges.groovy
@@ -3,14 +3,16 @@ def changes() {
 
     echo "Gathering SCM changes"
     def changeLogSets = currentBuild.changeSets
-    for (int i = 0; i < changeLogSets.size(); i++) {
-        def entries = changeLogSets[i].items
-        for (int j = 0; j < entries.length; j++) {
-            def entry = entries[j]
-            def files = new ArrayList(entry.affectedFiles)
-            for (int k = 0; k < files.size(); k++) {
-                def file = files[k]
-                sh """echo ${file.path}"""
+    if (changeLogSets != null) {
+        for (int i = 0; i < changeLogSets.size(); i++) {
+            def entries = changeLogSets[i].items
+            for (int j = 0; j < entries.length; j++) {
+                def entry = entries[j]
+                def files = new ArrayList(entry.affectedFiles)
+                for (int k = 0; k < files.size(); k++) {
+                    def file = files[k]
+                    sh """echo ${file.path}"""
+                }
             }
         }
     }
