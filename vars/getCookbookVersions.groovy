16389499e9c8d3079f7f9ba8521993e110743e6b
Daniel Bright <danielcbright@gmail.com>
2019-02-11 12:15:26 -0500

    adding cookbook version testing

--- vars/getCookbookVersions.groovy
+++ vars/getCookbookVersions.groovy
@@ -1,5 +1,7 @@
 def call() {
     wrap([$class: 'ChefIdentityBuildWrapper', jobIdentity: 'Jenkins']) {
+        unstash 'sslCert'
+        env.SSL_CERT_DIR= "${workspace}/.chef/trusted_certs/"
         def json = sh 'echo `knife environment show dbright-dev -F j`'
         def data = readJSON text json;
         echo data.cookbook_versions;
