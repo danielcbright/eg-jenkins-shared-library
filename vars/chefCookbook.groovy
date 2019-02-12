2f1326ccc2d694a8bfb7ba6884b147d975d2fc41
Daniel Bright <danielcbright@gmail.com>
2019-02-12 10:28:09 -0500

    bumping version

--- vars/chefCookbook.groovy
+++ vars/chefCookbook.groovy
@@ -127,6 +127,8 @@ pipeline {
       steps {
         wrap([$class: 'ChefIdentityBuildWrapper', jobIdentity: 'Jenkins']) {
         unstash 'cookbook'
+        unstash 'sslCert'
+        env.SSL_CERT_DIR= "${workspace}/.chef/trusted_certs/"
         sh 'berks install'
         sh 'berks update'
         sh 'berks upload'
