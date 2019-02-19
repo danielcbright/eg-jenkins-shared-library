9262ff764eb59853b50468a22ad57b7a4efb6973
Daniel Bright <danielcbright@gmail.com>
2019-02-19 12:37:33 -0500

    adding hub config for PR's

--- vars/chefCookbook.groovy
+++ vars/chefCookbook.groovy
@@ -132,7 +132,7 @@ pipeline {
         }
         git branch: 'master',
             credentialsId: 'd8135cad-2efa-46fa-bfb5-4aabdf9e2953',
-            url: 'ssh://git@github.com:danielcbright/eg-linux-role-cookbook-g.git'
+            url: 'git@github.com:danielcbright/eg-linux-role-cookbook-g.git'
         sh 'ls -alt'
        }
     }
