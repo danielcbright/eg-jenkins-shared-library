a644828d30d512e09e2f8907cfa762b4da545592
Daniel Bright <danielcbright@gmail.com>
2019-02-21 09:06:13 -0500

    adding cookbook stuff

--- vars/getCookbookVersions.groovy
+++ vars/getCookbookVersions.groovy
@@ -11,23 +11,22 @@ def call() {
         def cookbooks = [:]
         def envData = readJSON text: "${envJson}"
         for (element in envData.cookbook_versions) {
-            echo "${element.key} ${element.value}"
+            //echo "${element.key} ${element.value}"
             trimmedVer = element.value.substring(2)
-            cookbooks.put("${element.key}", "${element.value}")
-            // script {
-            //     cookbookJson = sh (
-            //         script: "knife cookbook show ${element.key} ${trimmedVer} -F j",
-            //         returnStdout: true
-            //     ).trim()
-            // }
-            // def cookbookData = readJSON text: "${cookbookJson}"
-            // echo cookbookData
-            // for (sourceURL in cookbookData.metadata.source_url) {
-            //     echo "${sourceURL.value}"
-            // }
+            cookbooks.put("${element.key}", "${trimmedVer}")
         }
         for (cookbook in cookbooks) {
             echo "${cookbook.key} ${cookbook.value}"
+            script {
+                cookbookJson = sh (
+                    script: "knife cookbook show ${cookbook.key} ${cookbook.value} -F j",
+                    returnStdout: true
+                ).trim()
+            }
+            def cookbookData = readJSON text: "${cookbookJson}"
+            for (sourceURL in cookbookData.metadata.source_url) {
+                echo "${sourceURL.value}"
+            }
         }
     }
 }
