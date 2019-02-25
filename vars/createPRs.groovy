bf3ba87ef509ea7bc7f25953afe4e1344e0cd27a
Daniel Bright <danielcbright@gmail.com>
2019-02-25 09:48:11 -0500

    adding createPRs step

--- vars/createPRs.groovy
+++ vars/createPRs.groovy
@@ -8,6 +8,7 @@ def call(String cookbookRepo, String dependCookbook, String newVersion) {
         new File("./${cookbookRepo}/.dir").mkdir()
     }
     dir ("./${cookbookRepo}") {
+        echo "${cookbookRepo}, ${dependCookbook}, ${newVersion}"
         git branch: 'master',
             credentialsId: 'd8135cad-2efa-46fa-bfb5-4aabdf9e2953',
             url: 'https://github.com/danielcbright/eg-linux-role-cookbook-g.git'
