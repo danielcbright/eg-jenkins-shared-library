65be52591f0ec87dc53cccb629f536e5470a73f6
Daniel Bright <danielcbright@gmail.com>
2019-02-25 14:19:49 -0500

    adding createPRs step

--- vars/createPRs.groovy
+++ vars/createPRs.groovy
@@ -5,7 +5,9 @@ def call(String cookbookInfo) {
         git branch: 'master',
             credentialsId: 'd8135cad-2efa-46fa-bfb5-4aabdf9e2953',
             url: "${cookbookRepo}"
-        sh "sed \"s/depends\\s'${dependCookbook}'.*/depends\\s'${dependCookbook}',\\s'=\\s${newVersion}'/g\" metadata.rb"
+        sh """
+        sed "s/depends\\s'${dependCookbook}'.*/depends\\s'${dependCookbook}',\\s'=\\s${newVersion}'/g" metadata.rb
+        """
         sh 'cat metadata.rb'
     }
 }
