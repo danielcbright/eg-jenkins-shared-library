38a79f15b2ba57586902db68cac1b7f528735dcc
Daniel Bright <danielcbright@gmail.com>
2019-02-25 08:13:10 -0500

    adding createPRs step

--- vars/chefSpec.groovy
+++ vars/chefSpec.groovy
@@ -2,6 +2,8 @@
 def call() {
     node {
         wrap([$class: 'ChefIdentityBuildWrapper', jobIdentity: 'Jenkins']) {
+        unstash 'sslCert'
+        env.SSL_CERT_DIR= "${workspace}/.chef/trusted_certs/"
         sh 'chef exec rspec'
         }
     }
