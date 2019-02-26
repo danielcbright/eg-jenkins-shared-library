6da42ea69d102aa996e365c3d0e7dfcc435d0811
Daniel Bright <danielcbright@gmail.com>
2019-02-26 08:14:02 -0500

    adding createPRs step

--- vars/getCookbookVersions.groovy
+++ vars/getCookbookVersions.groovy
@@ -54,9 +54,9 @@ def getSourceUrl(String cookbook, String version, String depName, String depVer)
     def cookbookData = readJSON text: "${cookbookJson}"
     def sourceURL = cookbookData.metadata.source_url
     echo "TEST 1:: ${depName}"
-    def x = cookbookData.metadata.dependencies.["{$depName}"]
+    def x = cookbookData.metadata.dependencies."{$depName}"
     println "${cookbookData.keySet()}"
-    println "${cookbookData.metadata.dependencies.keySet()}"
+    println "${cookbookData.metadata.dependencies.${depName}.keySet()}"
     echo "TEST 1:: value ${x}"
     if(x) {
         echo "TEST 2:: ${depName}"
