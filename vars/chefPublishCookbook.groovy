062145b9ca262ccd5ac7493b7706dbd91e23825e
Daniel Bright <danielcbright@gmail.com>
2019-03-06 13:05:13 -0500

    adding logic

--- vars/chefPublishCookbook.groovy
+++ vars/chefPublishCookbook.groovy
@@ -1,6 +1,7 @@
 def call() {
     wrap([$class: 'ChefIdentityBuildWrapper', jobIdentity: 'Jenkins']) {
         unstash 'cookbook'
+        sh 'tar -xvf cookboog.tar.gz'
         unstash 'sslCert'
         env.SSL_CERT_DIR= "${workspace}/.chef/trusted_certs/"
         sh 'ls -alt'
