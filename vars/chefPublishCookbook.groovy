56c01f9483262d10863968f6bc3aafb80a97fb7e
Daniel Bright <danielcbright@gmail.com>
2019-03-06 12:58:43 -0500

    adding logic

--- vars/chefPublishCookbook.groovy
+++ vars/chefPublishCookbook.groovy
@@ -3,6 +3,8 @@ def call() {
         unstash 'cookbook'
         unstash 'sslCert'
         env.SSL_CERT_DIR= "${workspace}/.chef/trusted_certs/"
+        sh 'ls -alt'
+        sh 'pwd'
         sh 'berks install'
         sh 'berks update'
         sh 'berks upload'
