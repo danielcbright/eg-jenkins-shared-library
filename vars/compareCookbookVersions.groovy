1ff02a4add2715d9732bf75b3f354141776c4d55
Daniel Bright <danielcbright@gmail.com>
2019-03-07 12:32:44 -0500

    added error

--- vars/compareCookbookVersions.groovy
+++ vars/compareCookbookVersions.groovy
@@ -25,8 +25,7 @@ def call () {
     if ( cookbookVersion > cookbookHighestVersionChef ) {
         echo "PASS: local cookbook version [${cookbookVersion}] is higher than Chef Server version [${cookbookHighestVersionChef}]"
     } else {
-        echo "FAIL: local cookbook version [${cookbookVersion}] is NOT higher than Chef Server version [${cookbookHighestVersionChef}]"
-        exit()
+        error "FAIL: local cookbook version [${cookbookVersion}] is NOT higher than Chef Server version [${cookbookHighestVersionChef}]"
     }
     return "${cookbookName}:${cookbookVersion}"
 }
