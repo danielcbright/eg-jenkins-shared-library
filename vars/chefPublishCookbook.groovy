99c21912638076a0c6f97bbdff8fbec917ef677e
Daniel Bright <danielcbright@gmail.com>
2019-03-08 10:11:52 -0500

    fixing berks stuff

--- vars/chefPublishCookbook.groovy
+++ vars/chefPublishCookbook.groovy
@@ -6,8 +6,19 @@ def call() {
         env.SSL_CERT_DIR= "${workspace}/.chef/trusted_certs/"
         sh 'ls -alt'
         sh 'pwd'
-        sh 'berks install'
-        sh 'berks update'
-        sh 'berks upload'
+        script {
+                if (fileExists('Berksfile.lock')) {
+                        sh 'rm Berksfile.lock'
+                }
         }
+        script {
+                if (fileExists('Berksfile')) {
+                        sh 'berks install'
+                        sh 'berks update'
+                        sh 'berks upload'
+                } else {
+                        error "Berksfile doesn't exist"
+                }
+        }
+    }
 }
