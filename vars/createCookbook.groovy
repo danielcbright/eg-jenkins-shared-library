570e42499c9de34eab5a744b7cfe219a4046dbfc
Daniel Bright <danielcbright@gmail.com>
2019-03-12 12:13:02 -0400

    troubleshooting

--- vars/createCookbook.groovy
+++ vars/createCookbook.groovy
@@ -1,56 +1,56 @@
 def call() {
-    def userInput = input(
-        id: 'userInput', message: 'Enter cookbook information:?',
-        parameters: [
-            string(defaultValue: 'None',
-                    description: 'Cookbook name',
-                    name: 'COOKBOOKNAME'),
-            string(defaultValue: 'None',
-                    description: 'Supported systems (https://docs.chef.io/config_rb_metadata.html under "supports" section)',
-                    name: 'SUPPORTS'),
-            string(defaultValue: 'None',
-                    description: 'Maintainer name',
-                    name: 'MAINTAINER'),
-            string(defaultValue: 'None',
-                    description: 'Maintainer email',
-                    name: 'MAINTAINER_EMAIL'),
-            string(defaultValue: 'None',
-                    description: 'License type (https://docs.chef.io/config_rb_metadata.html under "license" section)',
-                    name: 'LICENSE'),
-            string(defaultValue: 'None',
-                    description: 'Short description of the cookbook',
-                    name: 'SHORTDESCRIPTION'),
-            string(defaultValue: 'None',
-                    description: 'Longer description of the cookbook',
-                    name: 'LONGDESCRIPTION'),
-            string(defaultValue: '0.0.1',
-                    description: 'Initial cookbook version (default is 0.0.1)',
-                    name: 'COOKBOOKVERSION'),
-            string(defaultValue: 'None',
-                    description: 'Chef version supported (https://docs.chef.io/config_rb_metadata.html under "chef_version" section)',
-                    name: 'CHEFVERSION'),
-        ])
-    inputCOOKBOOKNAME       = userInput.COOKBOOKNAME?:''
-    inputSUPPORTS           = userInput.SUPPORTS?:''
-    inputMAINTAINER         = userInput.MAINTAINER?:''
-    inputMAINTAINER_EMAIL   = userInput.MAINTAINER_EMAIL?:''
-    inputLICENSE            = userInput.LICENSE?:''
-    inputSHORTDESCRIPTION   = userInput.SHORTDESCRIPTION?:''
-    inputLONGDESCRIPTION    = userInput.LONGDESCRIPTION?:''
-    inputCOOKBOOKVERSION    = userInput.COOKBOOKVERSION?:''
-    inputCHEFVERSION        = userInput.CHEFVERSION?:''
-
-    echo inputCOOKBOOKNAME
-    echo inputSUPPORTS
-    echo inputMAINTAINER
-    echo inputMAINTAINER_EMAIL
-    echo inputLICENSE
-    echo inputSHORTDESCRIPTION
-    echo inputLONGDESCRIPTION
-    echo inputCOOKBOOKVERSION
-    echo inputCHEFVERSION
-
     node {
+        def userInput = input(
+            id: 'userInput', message: 'Enter cookbook information:?',
+            parameters: [
+                string(defaultValue: 'None',
+                        description: 'Cookbook name',
+                        name: 'COOKBOOKNAME'),
+                string(defaultValue: 'None',
+                        description: 'Supported systems (https://docs.chef.io/config_rb_metadata.html under "supports" section)',
+                        name: 'SUPPORTS'),
+                string(defaultValue: 'None',
+                        description: 'Maintainer name',
+                        name: 'MAINTAINER'),
+                string(defaultValue: 'None',
+                        description: 'Maintainer email',
+                        name: 'MAINTAINER_EMAIL'),
+                string(defaultValue: 'None',
+                        description: 'License type (https://docs.chef.io/config_rb_metadata.html under "license" section)',
+                        name: 'LICENSE'),
+                string(defaultValue: 'None',
+                        description: 'Short description of the cookbook',
+                        name: 'SHORTDESCRIPTION'),
+                string(defaultValue: 'None',
+                        description: 'Longer description of the cookbook',
+                        name: 'LONGDESCRIPTION'),
+                string(defaultValue: '0.0.1',
+                        description: 'Initial cookbook version (default is 0.0.1)',
+                        name: 'COOKBOOKVERSION'),
+                string(defaultValue: 'None',
+                        description: 'Chef version supported (https://docs.chef.io/config_rb_metadata.html under "chef_version" section)',
+                        name: 'CHEFVERSION'),
+            ])
+        inputCOOKBOOKNAME       = userInput.COOKBOOKNAME?:''
+        inputSUPPORTS           = userInput.SUPPORTS?:''
+        inputMAINTAINER         = userInput.MAINTAINER?:''
+        inputMAINTAINER_EMAIL   = userInput.MAINTAINER_EMAIL?:''
+        inputLICENSE            = userInput.LICENSE?:''
+        inputSHORTDESCRIPTION   = userInput.SHORTDESCRIPTION?:''
+        inputLONGDESCRIPTION    = userInput.LONGDESCRIPTION?:''
+        inputCOOKBOOKVERSION    = userInput.COOKBOOKVERSION?:''
+        inputCHEFVERSION        = userInput.CHEFVERSION?:''
+
+        echo inputCOOKBOOKNAME
+        echo inputSUPPORTS
+        echo inputMAINTAINER
+        echo inputMAINTAINER_EMAIL
+        echo inputLICENSE
+        echo inputSHORTDESCRIPTION
+        echo inputLONGDESCRIPTION
+        echo inputCOOKBOOKVERSION
+        echo inputCHEFVERSION
+    
         dir ("${inputCOOKBOOKNAME}") {
             git branch: 'master',
                 credentialsId: 'd8135cad-2efa-46fa-bfb5-4aabdf9e2953',
