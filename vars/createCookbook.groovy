d0ba77b08ff275c7e90389955708e21d9100bcd8
Daniel Bright <danielcbright@gmail.com>
2019-03-12 10:51:02 -0400

    troubleshooting

--- vars/createCookbook.groovy
+++ vars/createCookbook.groovy
@@ -80,6 +80,7 @@ def call() {
             git add .
             git commit -m "Initial commit of ${inputCOOKBOOKNAME} by Jenkins"
             /usr/local/bin/hub create
+            git remote add origin https://\$USERNAME:\$GITHUB_TOKEN@github.com/danielcbright/${inputCOOKBOOKNAME}
             git push --set-upstream origin master
             """
     }
