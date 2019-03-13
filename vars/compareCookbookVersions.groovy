46360e5627bea9a12d625bfac6f864dc9f118e6f
Daniel Bright <danielcbright@gmail.com>
2019-03-13 08:59:41 -0400

    testing

--- vars/compareCookbookVersions.groovy
+++ vars/compareCookbookVersions.groovy
@@ -21,7 +21,6 @@ def call () {
             ).trim()
         }
     }
-    echo cookbookHighestVersionChef
     if (env.BRANCH_NAME != "master") {
         if ( cookbookVersion > cookbookHighestVersionChef ) {
             echo "PASS: local cookbook version [${cookbookVersion}] is higher than Chef Server version [${cookbookHighestVersionChef}]"
@@ -30,7 +29,7 @@ def call () {
             error "FAIL: local cookbook version [${cookbookVersion}] is NOT higher than Chef Server version [${cookbookHighestVersionChef}]"
         }
     } else {
-        if ( cookbookHighestVersionChef.contains("Cannot find a cookbook named ${cookbookName}") ) {
+        if ( cookbookHighestVersionChef == null ) {
             echo "Cookbook not on Chef Server, will prompt to upload"
             return "NOT ON SERVER"
         }
