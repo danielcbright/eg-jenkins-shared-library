82256af68e3af06b66fc4490f31814e40cd6405a
Daniel Bright <danielcbright@gmail.com>
2019-03-13 09:26:10 -0400

    testing

--- vars/compareCookbookVersions.groovy
+++ vars/compareCookbookVersions.groovy
@@ -34,7 +34,8 @@ def call () {
         if ( cookbookHighestVersionChef.contains("Cannot") ) {
             echo "Cookbook not on Chef Server, will prompt to upload"
             returnText = "NOT ON SERVER"
-        } else (env.BRANCH_NAME == "master") {
+        } else 
+            if (env.BRANCH_NAME == "master") {
             echo "Cookbook on Chef Server already, this is the master branch, so not prompting to upload"
             returnText = "${cookbookName}:${cookbookVersion}"
         } 
